1ebb5d9c25b12420ab4fb687640c82de37962034
Oliver Dworsky <omtgd123@gmail.com>
2022-11-06 16:15:23 -0800

    commit pls

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ function setup() {
     createCanvas(400, 400);
     console.log("Hello World")
     console.log("I worked on art for the game and planned out mechanics")
+    //I don't know why I can't commit yet
   }
   
   function draw() {
