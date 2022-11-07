c94018e149ce45bd833810327bbc8cea579bec64
Oliver Dworsky <omtgd123@gmail.com>
2022-11-06 16:12:02 -0800

    commited

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 function setup() {
     createCanvas(400, 400);
     console.log("Hello World")
+    console.log("I worked on art for the game and planned out mechanics")
   }
   
   function draw() {
