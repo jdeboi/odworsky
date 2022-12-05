d10bb9724732c1010f4574e7d11e4ba5478a50a6
Oliver Dworsky <omtgd123@gmail.com>
2022-12-05 10:42:00 -0800

    committy

--- Snow.js
+++ Snow.js
@@ -3,14 +3,31 @@ class Snow {
         noStroke()
         this.x = random(0, windowWidth)
         this.y = y
-        this.col = color(255, 255, 255)
+        this.col = 'white'
         this.w = random(0, 5)
-        this.speed = 1+dif
+        if(dif<3.5){
+            this.speed = 4-dif
+        }
+        else{
+            this.speed = .5
+        }
+        this.invCount = 0
     }
 
     display(){
         fill(this.col)
         rect(this.x, this.y, this.w)
+    }
+
+    inv(){
+        if(this.invCount>0){
+            this.invCount --
+            fill('black')
+            rect(this.x, this.y, this.w*20)
+        }
+        if(round(random(1,50)) == 1 && this.invCount == 0){
+            this.invCount += 100
+        }
     }
 
     move(){
--- sketch.js
+++ sketch.js
@@ -22,9 +22,11 @@ let dif = 1
 let d = 0
 let dAlready = false
 let lastSki = []
-let filterCoolDown = 0
+let invCoolDown = 0
 let hS
+let isInv = false
 let playing = false
+let doInvert = false
 
 
 
@@ -40,6 +42,7 @@ function setup() {
   textAlign(CENTER)
   textFont(myFont)
   fS = false
+  bridge = loadImage('assets/bridge.png')
   log = loadImage('assets/new assets/log.png')
   bigRock = loadImage('assets/bRock.png')
   tree = loadImage('assets/tree.png')
@@ -66,19 +69,19 @@ function setup() {
 }
 
 function draw() {
-  if(localStorage == undefined){
+  if (localStorage == undefined) {
     localStorage.setItem('Highscore', 0)
   }
   hS = localStorage.getItem('Highscore')
-  if(playing == false){
-  menuScreen()
+  if (playing == false) {
+    menuScreen()
   } else {
     playScreen()
   }
 
 }
 
-function playScreen(){
+function playScreen() {
   playing = true
   ski1.resize(44, 102)
   skiR.resize(58, 92)
@@ -108,26 +111,27 @@ function menuScreen() {
   cursor()
   displayHS()
   textSize(100)
-  text("SKI GAME",windowWidth/2, windowHeight / 2 -100)
+  text("SKI GAME", windowWidth / 2, windowHeight / 2 - 100)
   textSize(40)
-  text("Press Return to Play", windowWidth/2, windowHeight / 2 + 50)
-  if(keyPressed("Enter")){
+  text("Press Return to Play", windowWidth / 2, windowHeight / 2 + 50)
+  if (keyPressed("Enter")) {
     playing = true
   }
 }
 
 function displayHS() {
   textSize(50)
-  text("High Score:" + hS, windowWidth/2, windowHeight - 100)
+  text("High Score:" + hS, windowWidth / 2, windowHeight - 100)
 }
 
 function pauseScreen() {
   textSize(100)
-  text('Game Paused', windowWidth/2, windowHeight / 2 - 50)
+  text('Game Paused', windowWidth / 2, windowHeight / 2 - 50)
   textSize(50)
-  text('Press Return to Unpause', windowWidth/2, windowHeight / 2 + 50)
-  text('Press "M" to Return to Menu', windowWidth/2, windowHeight / 2 + 100)
-  if(keyPressed('m')){
+  text('Press Return to Unpause', windowWidth / 2, windowHeight / 2 + 50)
+  text('Press "M" to Return to Menu', windowWidth / 2, windowHeight / 2 + 100)
+  if (keyPressed('m')) {
+    restartGame()
     playing = false
   }
   textSize(40)
@@ -141,7 +145,7 @@ function deathScreen() {
   fill('black')
   textSize(40)
   text('Press Return to Restart', windowWidth / 2, windowHeight / 2 + 50)
-  if(keyPressed("Enter")){
+  if (keyPressed("Enter")) {
     restartGame()
   }
   text(score, windowWidth - 70, 50)
@@ -153,12 +157,65 @@ function run() {
     restartGame()
     key = 'lmao'
   }
-  else if(keyPressed("escape")){
-    restartGame()
+  else if (keyPressed("escape")) {
     playing = false
+    restartGame()
   }
-  else{  
+  else {
     getHS()
+    if (isTotem() || (invCoolDown <= 0 && isInv)) {
+      if (isTotem() && isInv == false) {
+        score += 1000
+        invCoolDown += 1000
+        isInv = true
+        totems = []
+        doInvert = true
+        inv(log)
+        inv(bigRock)
+        inv(tree)
+        inv(rockPatch)
+        inv(bridge)
+        // inv(skiR)
+        // inv(skiL)
+        // inv(ski1)
+        inv(tot)
+        inv(bg)
+        doInvert = false
+      }
+      else if (isTotem()) {
+        score += 1000
+        invCoolDown += 100
+        totems = []
+      }
+      if (invCoolDown == 0 && isInv) {
+        isInv = false
+        doInvert = true
+        inv(log)
+        inv(bigRock)
+        inv(tree)
+        inv(rockPatch)
+        inv(bridge)
+        // inv(skiR)
+        // inv(skiL)
+        // inv(ski1)
+        inv(tot)
+        inv(bg)
+        doInvert = false
+      }
+
+    }
+    if (invCoolDown > 0) {
+      invCoolDown--
+    }
+    if (isInv) {
+      // skiX+=random(-1,1)
+      if (speed > 1) {
+        speed -= .2
+      } else if (speed < -1) {
+        speed += .2
+      }
+
+    }
     getDif()
     image(bg, 0, getBgY(), windowWidth, windowWidth * 4)
     image(bg, 0, getBgY() + 4 * windowWidth, windowWidth, windowWidth * 4)
@@ -176,21 +233,50 @@ function run() {
     snowflakey()
     getScore()
     displayScore()
-    lastSki[1] = skiY - 1.5 - 1.1 * dif}
-  // if(isTotem()){
-  //   filter(THRESHOLD)
-  //   filterCoolDown += 100
-  // }
-  // if(filterCoolDown == 0){
+    lastSki[1] = skiY - 1.5 - 1.1 * dif
+  }
 
-  // }
 
 }
 
-function getHS(){
-  if (score > hS){
+
+function inv(img) {
+  if (doInvert) {
+
+    // Load the pixels
+    img.loadPixels();
+
+    // Loop through the pixels X and Y
+    for (let y = 0; y < img.height; y++) {
+      for (let x = 0; x < img.width; x++) {
+
+        // Calculate the pixel index
+        const index = (y * img.width + x) * 4;
+
+        // Get the red, green, and blue values
+        const r = img.pixels[index + 0];
+        const g = img.pixels[index + 1];
+        const b = img.pixels[index + 2];
+
+        // Invert the colors
+        img.pixels[index + 0] = 255 - r;
+        img.pixels[index + 1] = 255 - g;
+        img.pixels[index + 2] = 255 - b;
+
+      }
+    }
+
+    // We're finished working with pixels so update them
+    img.updatePixels();
+
+    // Don't do invert again until we set it to true on click;
+  }
+}
+
+function getHS() {
+  if (score > hS) {
     localStorage.clear()
-    localStorage.setItem("Highscore", score+2)
+    localStorage.setItem("Highscore", score + 2)
   }
 }
 
@@ -204,7 +290,7 @@ function restartGame() {
   dAlready = false
   totems = []
   lastSki = []
-  filterCoolDown = 0
+  invCoolDown = 0
   speed = 0
   obstacles = []
   trails = []
@@ -223,17 +309,17 @@ function isTotem() {
     rCorn = totem.x + totem.wid
     lCorn = totem.x
     if (d == 'l') {
-      if (lCorn < skiX + w - 10 && rCorn > skiX && round(skiY + h, -1) == round(totem.y + totem.h, -1)) {
+      if (lCorn < skiX + w - 10 && rCorn > skiX && round(skiY + h, -2) == round(totem.y + totem.h, -2)) {
         return (true)
       }
     }
     else if (d == 'r') {
-      if (lCorn < skiX + w && rCorn > skiX + 10 && round(skiY + h, -1) == round(totem.y + totem.h, -1)) {
+      if (lCorn < skiX + w && rCorn > skiX + 10 && round(skiY + h, -2) == round(totem.y + totem.h, -2)) {
         return (true)
       }
     }
     else if (d == 0) {
-      if (lCorn < skiX + w && rCorn > skiX && round(skiY + 152, -1) == round(obstacle.y + obstacle.h, -1)) {
+      if (lCorn < skiX + w && rCorn > skiX && round(skiY + 152, -2) == round(totem.y + totem.h, -2)) {
         return (true)
       }
     }
@@ -244,18 +330,19 @@ function totemAdd() {
   if (objCoolDown > 0) {
     objCoolDown--
   }
-  if (doesOccur(.05) && objCoolDown <= 0) {
+  if (doesOccur(.08) && objCoolDown <= 0) {
     totems.push(new Totem(random(0, windowWidth - 52), windowHeight + 64, 52, 64, tot, dif))
     if (dif < 4) {
-      objCoolDown += 400 - 100 * dif
+      objCoolDown += 400 - 50 * dif
     }
     else {
-      objCoolDown += 100
+      objCoolDown += 200
     }
   }
   for (totem of totems) {
     totem.display()
     totem.move()
+    totem.display()
     if (totem.y + totem.h < 0) {
       totems.shift()
     }
@@ -336,6 +423,9 @@ function isAlive() {
       else if (lCorn < skiX + w - 10 && rCorn > skiX && skiY + h < obstacle.y + obstacle.h && skiY + h > obstacle.y && obstacle.pic == log) {
         alive = false
       }
+      else if (windowWidth / 2 - 100 > skiX + w - 10 && windowWidth / 2 + 100 < skiX && skiY + h < obstacle.y + obstacle.h && skiY + h > obstacle.y && obstacle.pic == bridge) {
+        alive = false
+      }
     }
     else if (d == 'r') {
       if (lCorn + obstacle.wid / 2 - 20 < skiX + w && lCorn + obstacle.wid / 2 + 20 > skiX + 10 && round(skiY + h, -2) == round(obstacle.y + obstacle.h, -2) && obstacle.pic == tree) {
@@ -350,6 +440,9 @@ function isAlive() {
       else if (lCorn < skiX + w && rCorn > skiX + 10 && skiY + h < obstacle.y + obstacle.h && skiY + h > obstacle.y && obstacle.pic == log) {
         alive = false
       }
+      else if (windowWidth / 2 - 100 > skiX + w && windowWidth / 2 + 100 < skiX + 10 && skiY + h < obstacle.y + obstacle.h && skiY + h > obstacle.y && obstacle.pic == bridge) {
+        alive = false
+      }
     }
     else if (d == 0) {
       if (lCorn + obstacle.wid / 2 - 20 < skiX + w && lCorn + obstacle.wid / 2 + 20 > skiX && round(skiY + 152, -2) == round(obstacle.y + obstacle.h, -2) && obstacle.pic == tree) {
@@ -362,6 +455,9 @@ function isAlive() {
         alive = false
       }
       else if (lCorn < skiX + w && rCorn > skiX && skiY + h < obstacle.y + obstacle.h && skiY + h > obstacle.y && obstacle.pic == log) {
+        alive = false
+      }
+      else if (windowWidth / 2 - 100 > skiX + w && windowWidth / 2 + 100 < skiX && skiY + h < obstacle.y + obstacle.h && skiY + h > obstacle.y && obstacle.pic == bridge) {
         alive = false
       }
     }
@@ -390,10 +486,16 @@ function getScore() {
 }
 
 function displayScore() {
-  fill('black')
+  if (isInv) {
+    fill('white')
+  }
+  else {
+    fill('black')
+  }
   textFont(myFont)
   textSize(40)
   text(score, windowWidth - 70, 50)
+  fill('black')
 }
 
 function obstacleAdd() {
@@ -409,36 +511,43 @@ function obstacleAdd() {
       objCoolDown += 60
     }
   }
-  else if (doesOccur(.5) && objCoolDown <= 0) {
+  if (doesOccur(2) && objCoolDown <= 0) {
     obstacles.push(new Obstacle(random(0, windowWidth - 141), windowHeight + 177, 141, 177, tree, dif))
     if (dif < 4) {
-      objCoolDown += 400 - 15 * dif
+      objCoolDown += 500 - 5 * dif
     }
     else {
-      objCoolDown += 350
+      objCoolDown += 450
     }
   }
-  else if (doesOccur(.5) && objCoolDown <= 0) {
+  if (doesOccur(.3) && objCoolDown <= 0) {
     obstacles.push(new Obstacle(random(0, windowWidth - 116), windowHeight + 68, 116, 68, log, dif))
     if (dif < 4) {
-      objCoolDown += 200 - 35 * dif
+      objCoolDown += 200 - 5 * dif
     }
     else {
-      objCoolDown += 60
+      objCoolDown += 150
     }
   }
-  else if (doesOccur(.15) && objCoolDown <= 0) {
+  if (doesOccur(.1) && objCoolDown <= 0) {
     obstacles.push(new Obstacle(random(0, windowWidth - 159), windowHeight + 87, 159, 87, rockPatch, dif))
     if (dif < 4) {
       objCoolDown += 200 - 35 * dif
     }
-    else {
-      objCoolDown += 60
+    if (doesOccur(1) && objCoolDown <= 0) {
+      obstacles.push(new Obstacle((windowWidth-2700)/2, windowHeight + 2700, 2700, 2700, bridge, dif))
+      if (dif < 4) {
+        objCoolDown += 900 - 50 * dif
+      }
+      else {
+        objCoolDown += 500
+      }
     }
   }
   for (obstacle of obstacles) {
     obstacle.display()
     obstacle.move()
+    obstacle.display()
     if (obstacle.y + obstacle.h < 0) {
       obstacles.shift()
     }
@@ -483,7 +592,7 @@ function skimanDir() {
     h = 92
 
     if (speed > -3 - dif) {
-      speed -= .04 * dif
+      speed -= .08 * dif
     }
     image(skiL, skiX - 19, skiY, w, h)
   }
@@ -493,7 +602,7 @@ function skimanDir() {
     h = 92
 
     if (speed < 3 + dif) {
-      speed += .04 * dif
+      speed += .08 * dif
     }
     image(skiR, skiX - 10, skiY, w, h)
   }
@@ -539,7 +648,11 @@ function snowInt() {
 
 function snowflakey() {
   for (let snowflake of snowflakes) {
-    snowflake.display()
+    if (isInv) {
+      snowflake.inv()
+    } else {
+      snowflake.display()
+    }
     snowflake.move()
     let chanceOfFall = random(0, 10)
     if ((snowflake.getY() >= windowHeight) || chanceOfFall < .035) {
